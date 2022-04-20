1737c9a8690260d44f6de3c349ec1a1b37782a28
junwei-li <ljw532344863@sina.com>
2022-04-20 09:37:20 +0800

    Commit manually.

--- git.list.committer.email.sh
+++ git.list.committer.email.sh
@@ -2,6 +2,6 @@ function git-list-emails-of-current-branch() {
   git shortlog --summary --numbered --email
 }
 
-function git-list-emails-of-current-branch() {
+function git-list-emails-of-all-branch() {
   git shortlog --summary --numbered --email --all
 }
