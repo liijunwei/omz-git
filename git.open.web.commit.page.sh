aa8fb6b8df289706a2bfc0f9d4fdcd69fe2491ad
junwei-li <ljw532344863@sina.com>
2022-04-20 17:23:55 +0800

    Commit manually.

--- git.open.web.commit.page.sh
+++ git.open.web.commit.page.sh
@@ -27,3 +27,18 @@ EOF
   local open_in_web_tag=$2
   [[ -z "$open_in_web_tag" ]] && open $final_url;
 }
+
+# https://github.com/monacohq/monaco-rails/tree/chore/update_pry__silent-warning
+function git-view-branch-in-web() {
+  local branch=$1
+  local current_branch=$(git rev-parse --abbrev-ref HEAD)
+  local branch_name=${branch:-"$current_branch"}
+
+  local repo_http_url=$(get_repo_http_url)
+  local repo_without_dot_git=$(dirname $repo_http_url)/$(basename $repo_http_url .git)
+  local final_url="${repo_without_dot_git}/tree/${branch_name}"
+
+  cat <<EOF
+${final_url};
+EOF
+}
