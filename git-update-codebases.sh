970d9f1fa0e9c055400264654a46fcd23af39a5c
junwei <ljw532344863@sina.com>
2022-04-28 11:47:22 +0800

    Refactor update-all-codebases using GNU Parallel.
    + https://www.gnu.org/software/parallel

--- git-update-codebases.sh
+++ git-update-codebases.sh
@@ -1,27 +1,8 @@
-MASTER_OR_MAIN_BRANCHE=(
-  main
-  master
-)
+#!/bin/bash
 
-# TODO use https://www.gnu.org/software/parallel/
+# Prerequisite:
+#   brew install parallel
+#   brew install pygitup
 function update-all-codebases() {
-  local current_dir=$(pwd)
-
-  for project in $(ls -d */); do
-    cd $project
-
-    local result=$(git rev-parse --is-inside-work-tree &> /dev/null; echo $?)
-
-    if [ "$result" = "0" ]; then
-      echo "\e[1;31m[Updating $project]\e[0m"
-      echo "========================================="
-      git up
-    else
-      echo "\e[1;31m[Not a git repository: $(pwd)]\e[0m"
-    fi
-
-    cd ..
-  done
-
-  cd $current_dir
+  ls -d */ | parallel "cd {} && echo \"\e[1;31m[Updating {}]\e[0m\" && git up && echo"
 }
