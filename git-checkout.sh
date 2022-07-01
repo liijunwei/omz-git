e473f7d3497d6c926ac58e6e9d33a783bb131b35
junwei <ljw532344863@sina.com>
2022-07-01 17:47:53 +0800

    chore: refactor gbbc

--- git-checkout.sh
+++ git-checkout.sh
@@ -3,7 +3,12 @@
 alias 2='git checkout - && echo && git log | head -n1'
 
 function gbbc() {
-  git checkout $(git branch -vv | fzf | awk '{print $1}')
+  local keyword=$1
+  if [[ "" == "$keyword" ]]; then
+    git checkout $(git branch -vv | fzf | awk '{print $1}')
+  else
+    git checkout $(git branch -vv | fzf -q $keyword | awk '{print $1}')
+  fi
 }
 
 function gcob() {
