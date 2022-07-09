b78ba205ade59b26ee8aa38feae9e9d791f6857a
lijunwei <ljw532344863@sina.com>
2022-07-09 10:52:35 +0800

    Refactor git-setup-auto-commit: make interval configurable

--- git-commit.sh
+++ git-commit.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 function git-setup-auto-commit-push() {
-  watch -n 120 "git pull && git add . && git commit -m 'Commit automatically by watch' && git push"
+  echo "AUTO_COMMIT_INTERVAL=5 git-setup-auto-commit-push # Use this if you want to customize interval, default 300s"
+  local interval=${AUTO_COMMIT_INTERVAL:-300}
+  watch -n $interval "git pull && git add . && git commit -m 'Commit automatically by watch' && git push"
 }
 
 function git-setup-auto-commit() {
-  watch -n 120 "git pull && git add . && git commit -m 'Commit automatically by watch'"
+  echo "AUTO_COMMIT_INTERVAL=5 git-setup-auto-commit # Use this if you want to customize interval, default 300s"
+  local interval=${AUTO_COMMIT_INTERVAL:-300}
+  watch -n $interval "git pull && git add . && git commit -m 'Commit automatically by watch'"
 }
 
 function c() {
