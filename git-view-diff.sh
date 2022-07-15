17bc40ff0675b889df878c1d4a2778bd92f53166
junwei <ljw532344863@sina.com>
2022-07-15 14:53:34 +0800

    Add note and va

--- git-view-diff.sh
+++ git-view-diff.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note: feature branch need to rebase develop in a regular basis
+
 function v() {
   local keyword=$1
   if [[ "" == "$keyword" ]]; then
@@ -7,4 +9,8 @@ function v() {
   else
     $CORE_EDITOR $(git diff develop --name-only | fzf -q $keyword --preview 'bat --style=numbers --color=always --line-range :500 {}')
   fi
+}
+
+function va() {
+  $CORE_EDITOR $(git diff develop --name-only)
 }
