683a76b627f3117512ac47c962c90f662d1c8887
junwei <ljw532344863@sina.com>
2022-07-27 11:36:47 +0800

    Fix bug

--- git-check-diff-files.sh
+++ git-check-diff-files.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-function check-git-diff-files-with() {
-  local param_gem=$1
-  local param_base=$2
-  local default_base="origin/develop"
-  local base=${param_base:-"$default_base"}
+function check-git-diff-files-with-rspec() {
+  local branch=$1
 
   local diff_files=(
-    $(git diff --merge-base $base --name-only spec/**/*_spec.rb)
-    $(git diff --staged           --name-only spec/**/*_spec.rb)
+    $(git diff --merge-base $branch --name-only spec/**/*_spec.rb)
+    $(git diff --staged             --name-only spec/**/*_spec.rb)
   )
 
-  case $param_gem in
-    rspec)
-      bundle exec rspec ${diff_files[@]}
-      ;;
-    rubocop)
-      bundle exec rubocop -A ${diff_files[@]}
-      ;;
-    *)
-      echo "gem $param_gem not supported"
-      ;;
-  esac
+  if [ ${#diff_files[@]} -eq 0 ]; then
+    echo "No files detected"
+    return 0
+  else
+    echo "run for: ${diff_files[*]}"
+    bundle exec rspec ${diff_files[@]}
+  fi
+}
+
+function check-git-diff-files-with-rubocop() {
+  local branch=$1
+
+  local diff_files=(
+    $(git diff --merge-base $branch --name-only **/*.rb)
+    $(git diff --staged             --name-only **/*.rb)
+  )
+
+  if [ ${#diff_files[@]} -eq 0 ]; then
+    echo "No files detected"
+    return 0
+  else
+    echo "run for: ${diff_files[*]}"
+    bundle exec rubocop -A ${diff_files[@]}
+  fi
 }
