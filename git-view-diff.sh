46aa5bbc7e8f5e703bc74baf609d1c3b69d7edda
junwei <ljw532344863@sina.com>
2022-08-29 16:06:27 +0800

    Refactoring

--- git-view-diff.sh
+++ git-view-diff.sh
@@ -3,14 +3,13 @@
 # Note: feature branch need to rebase develop in a regular basis
 
 function v() {
-  local keyword=$1
-  if [[ "" == "$keyword" ]]; then
-    $CORE_EDITOR $(git diff develop --name-only | fzf --preview 'bat --style=numbers --color=always --line-range :500 {}')
-  else
-    $CORE_EDITOR $(git diff develop --name-only | fzf -q $keyword --preview 'bat --style=numbers --color=always --line-range :500 {}')
-  fi
+  local keyword=${1:-""}
+  local fork_point=$(git-fork-point-of develop)
+
+  $CORE_EDITOR $(git diff $fork_point --name-only | fzf -q "$keyword" --preview 'bat --style=numbers --color=always --line-range :500 {}')
 }
 
 function va() {
-  $CORE_EDITOR $(git diff develop --name-only)
+  local fork_point=$(git-fork-point-of develop)
+  $CORE_EDITOR $(git diff $fork_point --name-only)
 }
