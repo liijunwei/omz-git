6dd578f719cfefbdede0e2c97b7e82e0d48647d8
junwei <ljw532344863@sina.com>
2022-08-30 10:55:52 +0800

    Refactoring

--- git-checkout.sh
+++ git-checkout.sh
@@ -3,12 +3,8 @@
 alias 2='git checkout - && echo && git log | head -n1'
 
 function gbbc() {
-  local keyword=$1
-  if [[ "" == "$keyword" ]]; then
-    git checkout $(git branch -vv | fzf | awk '{print $1}')
-  else
-    git checkout $(git branch -vv | fzf -q $keyword | awk '{print $1}')
-  fi
+  local keyword=${1:-""}
+  git checkout $(git branch -vv | fzf -q "$keyword" | awk '{print $1}')
 }
 
 function gcob() {
