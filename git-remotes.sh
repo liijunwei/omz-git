62d5a83cd4d898cde04718fa6f1ee591a090a9cb
lijunwei <ljw532344863@sina.com>
2022-09-24 12:50:17 +0800

    Add git-change-origin-repo
    * how to work with your fork and upstream ?
    * https://stackoverflow.com/questions/11619593/how-to-change-the-fork-that-a-repository-is-linked-to

--- git-remotes.sh
+++ git-remotes.sh
@@ -18,3 +18,35 @@ function git-change-remote-repo() {
   git remote -v
   echo
 }
+
+# how to work with your fork and upstream?
+# https://stackoverflow.com/questions/11619593/how-to-change-the-fork-that-a-repository-is-linked-to
+function git-change-origin-repo() {
+  local neworigin=$1
+
+  if [ ! -z "$neworigin" ]; then
+    git remote rename origin upstream
+    echo "---------------------------------------------"
+    git branch -avv | cat
+    git remote add origin $neworigin
+  fi
+
+  cat <<EOF
+
+USAGE:
+  git-work-with-forked-repo
+  git-work-with-forked-repo your-new-origin
+
+Sync fork by:
+  git checkout main && git pull
+
+Develop on your fork by:
+  git checkout newFeatureBranch
+  # commit your changes
+  # sync fork
+  git rebase main
+  git push --force
+  # make a pull request
+
+EOF
+}
