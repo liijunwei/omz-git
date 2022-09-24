9d81c45a20d7c2bc23899e118ee2f5f93019bdff
lijunwei <ljw532344863@sina.com>
2022-09-24 12:52:38 +0800

    Remove git-change-remote-repo(replaced by git-change-origin-repo)

--- git-remotes.sh
+++ git-remotes.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# remove old remote and add new remote
-# e.g. git-change-remote-repo git@github.com:your-username/custom-omz-plugins.git
-function git-change-remote-repo() {
-  local neworigin=$1
-
-  echo "remote info(before)"
-  echo "=================="
-  git remote -v
-  echo
-
-  git remote remove origin
-  git remote add origin "$neworigin"
-
-  echo "remote info(after)"
-  echo "=================="
-  git remote -v
-  echo
-}
-
 function git-change-origin-repo() {
   local neworigin=$1
 
