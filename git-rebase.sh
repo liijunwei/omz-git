70d0081dcac289a00d328a0af0eddfa2317b9958
junwei <ljw532344863@sina.com>
2022-11-10 23:21:51 +0800

    Add todo and git-squash-to-merge-base

--- git-rebase.sh
+++ git-rebase.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# To be removed(seems never used)
 function grbu() {
   git rebase HEAD@{u}
 }
 
+# To be removed(seems never used)
 function grbiu() {
   GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/s/'" git rebase -i HEAD@{u}
+}
+
+function git-squash-to-merge-base() {
+  local branch=$1
+  local mb_commitid=$(git merge-base $branch HEAD)
+
+  GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/s/'" git rebase -i $mb_commitid
 }
