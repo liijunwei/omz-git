0bd2401e8a95d1445c2fd52dcf07ed3414e6a835
junwei <ljw532344863@sina.com>
2023-01-06 16:23:08 +0800

    Add todo

--- git-pull-request.sh
+++ git-pull-request.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO refactoring based on https://github.blog/2014-10-13-linking-merged-pull-requests-from-commits/
 function git-find-pull-request() {
   local keyword=$1
   local first_commitid=$(git log --pretty="format:%H" -S "$keyword" | tail -n1)
