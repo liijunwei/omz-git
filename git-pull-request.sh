2885d78673bf60fa73e2520124c068eade6da57c
junwei <ljw532344863@sina.com>
2023-01-26 15:53:21 +0800

    Refactor git-find-pull-request

--- git-pull-request.sh
+++ git-pull-request.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# TODO refactoring based on https://github.blog/2014-10-13-linking-merged-pull-requests-from-commits/
 function git-find-pull-request() {
   local keyword=$1
   local first_commitid=$(git log --pretty="format:%H" -S "$keyword" | tail -n1)
 
-  git-view-in-web $first_commitid
+  open "$(repo_http_url)/search?q=is:merged ${first_commitid}&type=issues"
 }
