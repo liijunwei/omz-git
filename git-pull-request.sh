70c42e0b7ef8d13e4d8b129c9402c0422aa17559
junwei <ljw532344863@sina.com>
2023-01-31 20:55:12 +0800

    Add log

--- git-pull-request.sh
+++ git-pull-request.sh
@@ -4,5 +4,7 @@ function git-find-pull-request() {
   local keyword=$1
   local first_commitid=$(git log --pretty="format:%H" -S "$keyword" | tail -n1)
 
+  echo "commitid found: $first_commitid"
+
   open "$(repo_http_url)/search?q=is:merged ${first_commitid}&type=issues"
 }
