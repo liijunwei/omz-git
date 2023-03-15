846fd92f982e71e6ece99c32d12cf9d562c6965a
junwei <ljw532344863@sina.com>
2023-03-15 12:34:04 +0800

    Automate my workflow

--- git-rebase.sh
+++ git-rebase.sh
@@ -6,3 +6,18 @@ function git-squash-to-merge-base() {
 
   GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/s/'" git rebase -i $mb_commitid
 }
+
+# work along with ./Rakefile
+function rddd() {
+  local mode=$1
+
+  if [[ "" == "$mode" ]]; then
+    echo "example:"
+    echo "  rddd rebase"
+    echo "  rddd reset"
+    echo "  rddd force_push"
+    return 1
+  fi
+
+  bundle exec rake -f ~/.oh-my-zsh/custom/plugins/omz-git/Rakefile rebase_branches mode=$mode
+}
