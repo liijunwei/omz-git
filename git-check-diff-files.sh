0aa226989dcb29091b1431bdcb324019f771a428
junwei <ljw532344863@sina.com>
2023-04-25 21:43:26 +0800

    Add check-git-diff-files-with-rspec-fail-fast

--- git-check-diff-files.sh
+++ git-check-diff-files.sh
@@ -15,6 +15,21 @@ function check-git-diff-files-with-rspec() {
   fi
 }
 
+function check-git-diff-files-with-rspec-fail-fast() {
+  local diff_files=(
+    $(git diff $(mb_commitid) --name-only spec/**/*_spec.rb)
+    $(git diff --staged --name-only spec/**/*_spec.rb)
+  )
+
+  if [ ${#diff_files[@]} -eq 0 ]; then
+    echo "No files detected"
+  else
+    set -x
+    bundle exec rspec --format=progress --fail-fast ${diff_files[@]}
+    set +x
+  fi
+}
+
 function check-git-diff-files-with-rubocop() {
   local diff_files=(
     $(git diff $(mb_commitid) --name-only **/*.rb)
