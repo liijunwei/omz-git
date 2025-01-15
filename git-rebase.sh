33957cb20c49ba43e838e599a565a4c0eeec40e1
junwei <ljw532344863@sina.com>
2025-01-15 15:23:58 +0800

    quick fixup commits

--- git-rebase.sh
+++ git-rebase.sh
@@ -4,7 +4,14 @@ function git-squash-to-merge-base() {
   local branch=$1
   local mb_commitid=$(git merge-base $branch HEAD)
 
-  GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/s/'" git rebase -i $mb_commitid
+  GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/squash/'" git rebase -i $mb_commitid
+}
+
+function git-squash-to-merge-base-v2() {
+  local branch=$1
+  local mb_commitid=$(git merge-base $branch HEAD)
+
+  GIT_EDITOR="gsed -i '2,/^$/s/^pick\b/fixup/'" git rebase -i $mb_commitid
 }
 
 # work along with https://github.com/liijunwei/rebase_pusher
